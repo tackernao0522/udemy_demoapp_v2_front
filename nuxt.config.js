60165771dae1256e04c16208d197503ff3af940b
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-03-18 20:56:16 +0900

    48 nuxt-i18nを導入する

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,9 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    // Doc: https://www.npmjs.com/package/@nuxtjs/i18n
+    '@nuxtjs/i18n'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -67,6 +69,29 @@ export default {
           error: 'FB8678',
           background: 'f6f6f4'
         }
+      }
+    }
+  },
+  // Doc: https://nuxt-community.github.io/nuxt-i18n/basic-usage.html#nuxt-link
+  i18n: {
+    // 対応言語
+    locales: ['ja', 'en'],
+    // デフォルトで使用する言語を指定
+    defaultLocale: 'ja',
+    // Doc: https://kazupon.github.io/vue-i18n/api/#properties
+    vueI18n: {
+      // 翻訳対象のキーがない場合に参照される言語
+      // "login": "ログイン"
+      fallbackLocale: 'ja',
+      // true => i18nの警告を完全に表示しない(default: false)
+      // silentTranslationWarn: true,
+      // フォールバック時に警告を発生させる(default: false)
+      // true => 警告を発生させない(翻訳のキーが存在しない場合のみ警告)
+      silentFallbackWarn: true,
+      // 翻訳データ
+      messages: {
+        ja: require('./locales/ja.json'),
+        en: require('./locales/en.json')
       }
     }
   },
