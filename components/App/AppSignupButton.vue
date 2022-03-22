39f84fa215216b9ea9a49d03a9ec8b4281c45774
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-03-22 17:36:15 +0900

    52 [ウェルカムページ 4/4] レスポンシブデザインに対応させよう

---
+++ components/App/AppSignupButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn
+    outlined
+    color="appblue"
+    class="ml-2 font-weight-bold"
+    to="/signup"
+  >
+    {{ $t('pages.signup') }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+
+}
+</script>
