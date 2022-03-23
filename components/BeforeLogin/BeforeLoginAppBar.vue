83a890b127d4a960455b269981ffe26b0fcab8cd
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-03-23 13:44:00 +0900

    54 ログイン前のレイアアウトファイルを作成する

---
+++ components/BeforeLogin/BeforeLoginAppBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app-bar
+    app
+    :height="homeAppBarHeight"
+    color="white"
+  >
+    <nuxt-link
+      to="/"
+      class="text-decoration-none"
+    >
+      <app-logo />
+    </nuxt-link>
+
+    <app-title
+      class="hidden-mobile-and-down"
+    />
+
+    <v-spacer />
+
+    <app-signup-button />
+    <app-login-button />
+  </v-app-bar>
+</template>
+
+<script>
+import AppLoginButton from '../App/AppLoginButton.vue'
+import AppSignupButton from '../App/AppSignupButton.vue'
+export default {
+  components: { AppSignupButton, AppLoginButton },
+  data ({ $store }) {
+    return {
+      homeAppBarHeight: $store.state.styles.homeAppBarHeight
+    }
+  }
+}
+</script>
