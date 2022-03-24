3aa9374d8d60da54cb9252cfe30a0cd2ac8e8061
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-03-24 20:26:04 +0900

    56 親子コンポーネント間の双方向バインディングを理解する

--- components/User/UserFormName.vue
+++ components/User/UserFormName.vue
@@ -1,5 +1,10 @@
 <template>
+  <!-- v-bind:value="name" => 描写 -->
+  <!-- v-on:input => インプットがあった時に発火する -->
+  <!-- $emit('キー(自由に決められる)', $event) => 子から親へデータを送信 -->
+  <!-- syncを使った場合、$emit('update:name', $event)とする -->
   <v-text-field
+    v-model="setName"
     label="ユーザー名を入力"
     placeholder="あなたの表示名"
     outlined
@@ -8,6 +13,21 @@
 
 <script>
 export default {
-
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    setName: {
+      get () {
+        return this.name
+      },
+      set (newValue) {
+        return this.$emit('update:name', newValue)
+      }
+    }
+  }
 }
 </script>
