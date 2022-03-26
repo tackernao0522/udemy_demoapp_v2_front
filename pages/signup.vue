8e2a77dda65e222c5e2cdd1ef660bbddedf61388
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-03-26 19:39:25 +0900

    59 ログインページを作成

--- pages/signup.vue
+++ pages/signup.vue
@@ -2,6 +2,7 @@
   <user-form-card>
     <template #user-form-card-content>
       <v-form
+        ref="form"
         v-model="isValid"
       >
         <user-form-name
@@ -17,15 +18,16 @@
         />
         <!-- disabled=true => ボタンクリックを無効にする -->
         <v-btn
-          :disabled="!isValid"
+          :disabled="!isValid || loading"
+          :loading="loading"
           block
           color="appblue"
           class="white--text"
+          @click="signup"
         >
           登録する
         </v-btn>
       </v-form>
-      {{ params }}
     </template>
   </user-form-card>
 </template>
@@ -43,7 +45,23 @@ export default {
     return {
       name: '',
       isValid: false,
+      loading: false,
       params: { user: { name: '', email: '', password: '' } }
+    }
+  },
+  methods: {
+    signup () {
+      this.loading = true
+      setTimeout(() => {
+        this.formReset()
+        this.loading = false
+      }, 1500)
+    },
+    formReset () {
+      this.$refs.form.reset()
+      for (const key in this.params.user) {
+        this.params.user[key] = ''
+      }
     }
   }
 }
