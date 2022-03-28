a161e6652a02f2a9fe78f5862087523aed75bc4a
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-03-28 17:15:34 +0900

    65 動的なルートに表示する個別プロジェクトページを作成する

---
+++ layouts/project.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <logged-in-app-bar />
+    <v-main>
+      project.vue
+      <Nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import LoggedInAppBar from '../components/LoggedIn/LoggedInAppBar.vue'
+export default {
+  components: { LoggedInAppBar }
+
+}
+</script>
