443c9248077c3048fc7a4bebfbc96ee030240441
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-04-07 21:05:29 +0900

    add handling Error Method

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,17 +1,21 @@
-export default ({ $axios, $auth }) => {
+export default ({ $axios, $auth, isDev }) => {
   // リクエストログ
   $axios.onRequest((config) => {
     config.headers.common['X-Requested-With'] = 'XMLHttpRequest'
     if ($auth.token) {
       config.headers.common.Authorization = `Bearer ${$auth.token}`
     }
-    // eslint-disable-next-line no-console
-    console.log(config)
+    if (isDev) {
+      // eslint-disable-next-line no-console
+      console.log(config)
+    }
   })
   // レスポンスログ
   $axios.onResponse((config) => {
-    // eslint-disable-next-line no-console
-    console.log(config)
+    if (isDev) {
+      // eslint-disable-next-line no-console
+      console.log(config)
+    }
   })
   // エラーログ
   $axios.onError((e) => {
